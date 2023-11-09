4be56b930a3467dde4ac297c6f70e492efd89e9f
April2737 <142591747+April2737@users.noreply.github.com>
2023-11-09 19:44:57 +0000

    Update and rename qutrit_planar_povm_max_bloch_vector_dichotomic_like_children.m to qutrit_planar_povm_max_bloch_vector_dichotomic_children.m

--- qutrit_planar_povm_max_bloch_vector_dichotomic_children.m
+++ qutrit_planar_povm_max_bloch_vector_dichotomic_children.m
@@ -1,4 +1,4 @@
-%   description: Search the maximal length of the Bloch vector eta that makes N rotationally symmetrical planar dichotomic-like children POVMs 
+%   description: Search the maximal length of the Bloch vector eta that makes N rotationally symmetrical planar dichotomic children POVMs 
 %   compatible by SDP.
 %   
 %   authors: Jiaxuan Zhang, Eric Chitambar
@@ -6,7 +6,7 @@
 %   requires: This MATLAB code is based on "J. Bavaresco, M. T. Quintino, L. Guerini, T. O. Maciel, D. Cavalcanti, and M. Terra Cunha, 
 %   https://git.io/v9znv (2017)". Download the codes and the required tools from this link to use this code.
 %
-%   last update: August, 2023
+%   last update: November, 2023
 
 rho_AB = [1/3,0,0,0,1/3,0,0,0,1/3; 
     0,0,0,0,0,0,0,0,0; 
@@ -16,10 +16,10 @@ rho_AB = [1/3,0,0,0,1/3,0,0,0,1/3;
     0,0,0,0,0,0,0,0,0; 
     0,0,0,0,0,0,0,0,0; 
     0,0,0,0,0,0,0,0,0; 
-    1/3,0,0,0,1/3,0,0,0,1/3];   
+    1/3,0,0,0,1/3,0,0,0,1/3];   % maximally entangled state
 
-N = 6;   % number of childrens
-k = 3;    % number of elements in a children POVM
+N=11;   % number of childrens
+k=2;    % number of outcomes in a children
 dA=3;   % dimension of system A
 dB=3;   % dimension of system B
 
@@ -34,19 +34,17 @@ Y3 = [0, 0, 0; 0, 0, -1i*c_GM; 0, 1i*c_GM, 0];   % lambda 7
 Z1 = [c_GM, 0, 0; 0, -c_GM, 0; 0, 0, 0];      % lambda 3
 Z2 = [c_GM/(3)^.5, 0, 0; 0, c_GM/(3)^.5, 0; 0, 0, -2*c_GM/(3)^.5];      % lambda 8
 
-M1 = X1;   % x axis
+M1 = X2;   % x axis
 M2 = Z2;   % y axis
 
 A = zeros(dA,dA,N,k);   % children POVM elements
 
 % Generate childrens
 for i=1:N
-    A(:,:,i,1) = (1/dA)*(eye(dA)) + (1/2)*(cos((i-1)*(2*pi)/(2*N)) * M1 + sin((i-1)*(2*pi)/(2*N)) * M2)    %#ok<NOPTS> 
-    A(:,:,i,2) = (1/dA)*eye(dA)   %#ok<NOPTS> 
-    A(:,:,i,3) = eye(dA) - A(:,:,i,1) - A(:,:,i,2)    %#ok<NOPTS> 
+    A(:,:,i,1) = (3/2)*((1/dA)*(eye(dA)) + (1/2)*(cos((i-1)*(2*pi)/(2*N)) * M1 + sin((i-1)*(2*pi)/(2*N)) * M2))   %#ok<NOPTS> 
+    A(:,:,i,2) = eye(dA) - A(:,:,i,1)    %#ok<NOPTS> 
 end 
 
 [F_ax, eta, flag] = WNR_sdp1(A,rho_AB);   % SDP
 
 eta    %#ok<NOPTS> 
-
